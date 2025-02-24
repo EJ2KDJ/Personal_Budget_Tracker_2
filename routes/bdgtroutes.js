864e1c8683a2678bf63c85a1bad92f25a7f9f006
EJ2KDJ <emmanuelle.salga2007@gmail.com>
2025-02-24 15:42:53 +0800

    return value error in delete transactions route

--- routes/bdgtroutes.js
+++ routes/bdgtroutes.js
@@ -221,7 +221,7 @@ router.delete('/transactions/:id', async (req, res) => {
     try {
         const { id } = req.params;
 
-        const result = await pool.query('DELETE FROM transactions WHERE id = $1', [id]);
+        const result = await pool.query('DELETE FROM transactions WHERE id = $1 RETURNING *', [id]);
 
         if (result.rows.length[0]) {
             return res.status(404).json({ error: "Transaction not found."});
