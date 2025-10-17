e2325ad24cf37e5fc9db5a33b05533ec7c3a3008
EJ2KDJ <65149114+EJ2KDJ@users.noreply.github.com>
2025-10-17 13:49:39 +0800

    trying to fix router error

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const transactionController = require('../controllers/transactionsController');
 const {authentication, requireAdmin} = require('../middleware');
 
