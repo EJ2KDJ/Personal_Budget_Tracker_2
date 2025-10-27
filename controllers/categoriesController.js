500f64d62fd11b9f3f441b3b33e35a7a742939f4
EJ2KDJ <65149114+EJ2KDJ@users.noreply.github.com>
2025-10-27 17:28:33 +0800

    Update categoriesController.js

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -12,7 +12,7 @@ const getCategoriesByUserId = async (req, res) => {
             return res.status(404).json({ error: 'User not found' });
         }
 
-        const categories = await Categories.findAll({ where: { user_id: userId } });
+        const categories = await Category.findAll({ where: { user_id: userId } });
         return res.status(200).json({ categories });
     } catch (err) {
         res.status(500).json({ error: err.message});
@@ -33,7 +33,7 @@ const createCategory = async (req, res) => {
             return res.status(400).json({ error: 'name and type are required' });
         }
 
-        const newCategory = await Categories.create({
+        const newCategory = await Category.create({
             user_id: userId,
             name,
             type
@@ -54,7 +54,7 @@ const deleteCategory = async (req, res) => {
         }
 
         const category_id = req.params.id;
-        const category = await Categories.findOne({ where: { id: category_id, user_id: userId } });
+        const category = await Category.findOne({ where: { id: category_id, user_id: userId } });
         if (!category) {
             return res.status(404).json({ error: 'Category not found' });
         }
